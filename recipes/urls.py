aac434c400de3d52bc0461fa3ae04f29bf3a31a1
jpmagalhaes12 <j.magalhaes1450@gmail.com>
2023-04-14 14:11:04 -0300

    caminhos

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<id>', views.recipe),
+    path('', views.home, name="home"),
+    path('recipes/<id>', views.recipe, name="recipe"),
 ]
