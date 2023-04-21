14cdd4d999afaed01aa0845364add97b67d7abcc
jpmagalhaes12 <j.magalhaes1450@gmail.com>
2023-04-20 21:37:25 -0300

    test home carrega recipes

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
+from recipes.models import Category, Recipe, User
 
 
 class RecipeViewsTest(TestCase):
@@ -22,6 +23,37 @@ class RecipeViewsTest(TestCase):
             'No recipes found',
             response.content.decode('utf-8')
         )
+
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name='Category')
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='username@email.com',
+        )
+        recipe = Recipe.objects.create(
+            category=category,
+            author=author,
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='Recipe-slug',
+            preparation_time='10',
+            preparation_time_unit='Minutos',
+            servings='5',
+            servings_unit='Porções',
+            preparation_steps='Recipé Prepation Steps',
+            preparation_steps_is_html=False,
+            is_published=True,
+        )
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+        self.assertIn('Recipe Title', content)
+        self.assertIn('10 Minutos', content)
+        self.assertIn('5 Porções', content)
+        self.assertEqual(len(response_context_recipes), 1)
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category',
